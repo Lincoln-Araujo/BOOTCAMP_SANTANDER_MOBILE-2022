dc684a423a7e74a44b2b0a1af7154872260d7d08
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-16 20:21:01 -0300

    criando classe pessoa

---
+++ BANK/Pessoa.kt
@@ -0,0 +1,10 @@
+class Pessoa {
+    var nome: String = "Duncan"
+    var cpf: String = "415326894"
+}
+
+fun main() {
+    val duncan = Pessoa()
+    println(duncan.nome)
+    println(duncan.cpf)
+}
