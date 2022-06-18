fe58488c6c1e50aef2c9c4d84cbd4d77ecea8b21
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-18 12:36:39 -0300

    criando lista mutável

---
+++ API-COLLECTIONS/TesteColecoesMutaveis.kt
@@ -0,0 +1,38 @@
+fun main() {
+    val duncan = Funcionario("Duncan", 10000.0, "PJ")
+    val lincoln = Funcionario("Lincoln", 8000.0, "CLT")
+    val dunkin = Funcionario("Dunkin", 9000.0, "PJ")
+
+    val funcionarios = mutableListOf(duncan, dunkin)
+    funcionarios.forEach {println(it)}
+
+    println("____________")
+    funcionarios.add(lincoln)
+    funcionarios.forEach { println(it)}
+
+    println("____________")
+    funcionarios.remove(dunkin)
+    funcionarios.forEach { println(it) }
+
+    println("_____________")
+    val funcionariosSet = mutableSetOf(duncan)
+    funcionariosSet.add(lincoln)
+    funcionariosSet.add(dunkin)
+    funcionariosSet.forEach{println(it)}
+
+    println("____________")
+    funcionariosSet.remove(dunkin)
+    funcionariosSet.forEach { println(it)}
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salario: $salario
+        """.trimIndent()
+}
