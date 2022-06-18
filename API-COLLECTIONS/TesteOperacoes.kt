600a7bce3af222436ebebd43842a1ee49a78cb25
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-18 07:15:41 -0300

    operações com arrays max, min, average e filter

---
+++ API-COLLECTIONS/TesteOperacoes.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios) {
+        println(salario)
+    }
+
+    println("_____________")
+    println("Maior salario: ${salarios.maxOrNull()}")
+    println("Menor salario: ${salarios.minOrNull()}")
+    println("Media salario: ${salarios.average()}")
+
+    val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    println("_____________")
+    salariosMaiorQue2500.forEach { println(it)}
+}
