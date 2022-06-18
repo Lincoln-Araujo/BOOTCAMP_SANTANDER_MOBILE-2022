388e915d037c949884b52d9475a9269a66d3a3a9
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-18 13:39:14 -0300

    criando funções extendidas

---
+++ API-COLLECTIONS/TesteFuncoeExtendida.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val salarios = arrayOf(
+        "2000".toBigDecimal(),
+        "1500".toBigDecimal(),
+        "4000".toBigDecimal()
+    )
+
+    println(salarios.somatoria())
+
+    println("___________")
+    println(salarios.media())
+
+}
