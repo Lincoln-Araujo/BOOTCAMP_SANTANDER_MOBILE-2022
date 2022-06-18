78ab5a38b59efe589e95b98e5bdc32622a2c7be5
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-18 07:56:08 -0300

    organizando elementos de uma lista por grupo

--- API-COLLECTIONS/TesteList.kts
+++ API-COLLECTIONS/TesteList.kts
@@ -1,7 +1,7 @@
 fun main(){
-    val duncan = Funcionario("Duncan", 10000.0)
-    val lincoln = Funcionario("Lincoln", 8000.0)
-    val dunkin = Funcionario("Dunkin", 9000.0)
+    val duncan = Funcionario("Duncan", 10000.0, "PJ")
+    val lincoln = Funcionario("Lincoln", 8000.0, "CLT")
+    val dunkin = Funcionario("Dunkin", 9000.0, "PJ")
 
     val funcionarios = listOf(duncan, lincoln, dunkin)
 
@@ -16,11 +16,18 @@ fun main(){
     funcionarios
         .sortedBy { it.salario }
         .forEach { println(it) }
+
+    println("______________")
+    // imprimindo uma lista organizada de acordo com o tipo de contratacao
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach {println(it)}
 }
 
 data class Funcionario(
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 ) {
     override fun toString(): String =
         """
