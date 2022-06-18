7c14b8fd3f007e89449b949276833404da776fca
Lincoln-Araujo <lincoln.fa@hotmail.com>
2022-06-18 08:15:58 -0300

    criando conjuntos com setOf e aplicando operações nesses conjuntos

---
+++ API-COLLECTIONS/TesteSet.kt
@@ -0,0 +1,36 @@
+fun main() {
+    val duncan = Funcionario("Duncan", 10000.0, "PJ")
+    val lincoln = Funcionario("Lincoln", 8000.0, "CLT")
+    val dunkin = Funcionario("Dunkin", 9000.0, "PJ")
+
+    val funcionarios = setOf(duncan, lincoln)
+    val funcionarios2 = setOf(dunkin)
+
+    // unindo list
+    val resultUnion = funcionarios.union(funcionarios2)
+    resultUnion.forEach {println(it)}
+
+    println("__________")
+    val funcionarios3 = setOf(duncan, lincoln, dunkin)
+    // achar um elemento de um subconjunto dentro de um outro conjunto
+    // neste caso, procuramos os elementos do subconjunto funcionarios 2
+    val resultSubtract = funcionarios3.subtract(funcionarios2)
+    resultSubtract.forEach {println(it)}
+
+    println("__________")
+    // acha um elemento em comum entre os conjuntos
+    val resultIntersect = funcionarios3.intersect(funcionarios2)
+    resultIntersect.forEach { println(it)}
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome: $nome
+            Salario: $salario
+        """.trimIndent()
+}
